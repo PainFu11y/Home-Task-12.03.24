01b6049b540eb17e150943ff22e2650aac4cc729
Painfu11y <333vahe777@gmail.com>
2024-03-10 20:51:07 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,177 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.println("Nermuceq orva hamar@ :");
+//        int day = 1;
+//        switch (day){
+//            case 2:;
+//            case 4:;
+//            case 6: System.out.println("Dasi");break;
+//            case 1:;
+//            case 3:;
+//            case 5: System.out.println("Dasi chenq");break;
+//            case 7: System.out.println("Kiraki"); break;
+//            default: System.out.println("mi ban sxal es gre");
+
+
+
+        //21
+//        int a = 1, b = 2, c = 3;
+//        if(a > b && a > c) System.out.println("max = " + a);
+//        else if (b > a && b > c) System.out.println("max = " + b);
+//        else System.out.println("max = " + c);
+
+        //22
+//        int a = 1, b = 2, c = 3;
+//        if(a < b && a < c) System.out.println("min = " + a);
+//        else if (b < a && b < c) System.out.println("min = " + b);
+//        else System.out.println("min = " + c);
+
+        //23
+//        int a = 1, b = 2, c = 3;
+//        if(a==1 || b==1 || c==1) System.out.println(true);
+//        else System.out.println(false);
+
+        //24
+//        int a = 1, b = 2, c = 3;
+//        if((a == 2 && b == 2)|| (a == 2 && c == 2)|| (b == 2 && c == 2)) System.out.println(true);
+//        else System.out.println(false);
+
+        //25
+//        int a = 3, b = 4, c = 5;
+//        if ((a+b >= c) && (a+c >= b) && (b+c >= a)) System.out.println("y=1");
+//        else System.out.println("y=2");
+
+        //26
+//        int a = 3, b = 4, c = 5;
+//                if ((a%2==0)||(b%2==0)||(c%2==0)) System.out.println(1);
+//                else System.out.println(2);
+
+        //27
+//        int a = 3, b = 4, c = 5;
+//        if(b-a == c-b) System.out.println(true);
+//        else System.out.println(false);
+
+        //28
+//        double a = 3, b = 4, c = 5;
+//        if (b/a == c/b) System.out.println(true);
+//        else System.out.println(false);
+
+        //29
+//        int a = 3, b = 4, c = 5;
+//        if (a<b){
+//            if (a<c) {
+//                if (b<c) System.out.println(a +" "+ b +" "+ c);
+//                else System.out.println(a +" "+ c +" "+ b);
+//            }else System.out.println(c +" "+ a +" "+ b);
+//        }else if (b<c) {
+//            if (a < c) System.out.println(b + " " + a + " " + c);
+//            else System.out.println(b + " " + c + " " + a);
+//        }else if(c<a){
+//            System.out.println(c + " " + b + " " + a);
+//        }
+
+        //30
+//        int a = 3, b = 4, c = 5;
+//        if (a>b){
+//            if (a>c) {
+//                if (b>c) System.out.println(a +" "+ b +" "+ c);
+//                else System.out.println(a +" "+ c +" "+ b);
+//            }else System.out.println(c +" "+ a +" "+ b);
+//        }else if (b>c) {
+//            if (a > c) System.out.println(b + " " + a + " " + c);
+//            else System.out.println(b + " " + c + " " + a);
+//        }else if(c>a){
+//            System.out.println(c + " " + b + " " + a);
+//        }
+
+        //31
+//        int a = 3, b = 4, c = 5, d = 6;
+//        int max = a;
+//        max = max > b ? max :  b;
+//        max = max > c ? max : c;
+//        max =  max > d ? max : d;
+//        System.out.println(max);
+
+        //32
+//        int a = 3, b = 4, c = 5, d = 6;
+//        int min = a;
+//        min = min < b ? min :  b;
+//        min = min < c ? min : c;
+//        min =  min < d ? min : d;
+//        System.out.println(min);
+
+        //33
+//        int a = 3, b = 4, c = 5, d = 6;
+//        if(a == 1||b == 1||c==1||d==1) System.out.println(true);
+//        else System.out.println(false);
+
+        //34
+//        int a = 3, b = 4, c = 5, d = 6;
+//        if(a+b == c+d) System.out.println(true);
+//        else if (b+c == a+d) System.out.println(true);
+//        else System.out.println(false);
+
+        //35
+//          int a = 3, b = 4, c = 5, d = 6;
+//          if (a == b+c+d) System.out.println(true);
+//          else if (b == a+c+d) System.out.println(true);
+//          else if(c == a+b+d) System.out.println(true);
+//          else if (d == a+b+c) System.out.println(true);
+//          else System.out.println(false);
+
+        //36
+//           int a = 3, b = 4, c = 5, d = 6;
+//        if ((a%2!=0 && b%2!=0)||(a%2!=0 && c%2!=0)||(a%2!=0 && d%2!=0)||
+//                (b%2!=0 && c%2!=0)||(b%2!=0 && d%2!=0)||(d%2!=0 && c%2!=0))
+//            System.out.println(true);
+//        else System.out.println(false);
+
+        //37
+//        int a = 3, b = 4, c = 5, d = 6;
+//        if((b-a == c-b) && (d-c == c-b)) System.out.println(true);
+//        else System.out.println(false);
+
+        //38
+//        double a = 3, b = 4, c = 5, d = 6;
+//        if(b/a == c/b && c/b == d/c) System.out.println(true);
+//        else System.out.println(false);
+
+        //39
+//        int a = 3, b = 34, c = -5, d = 6;
+//        int[] arr = {a,b,c,d};
+//        Arrays.sort(arr);
+//        for(int tver : arr){
+//            System.out.println(tver + " ");
+//        }
+
+        //40
+//        int a = 3, b = 34, c = -5, d = 6,u = 324;
+//        int[] arr = {a,b,c,d,u};
+//        Arrays.sort(arr);
+//        int left = 0;
+//        int right = arr.length - 1;
+//        while (left < right) {
+//            int temp = arr[left];
+//            arr[left] = arr[right];
+//            arr[right] = temp;
+//
+//            left++;
+//            right--;
+//        }
+//        for(int tver : arr){
+//            System.out.println(tver + " ");
+//        }
+
+
+
+
+
+
+
+    }
+}
+
+
