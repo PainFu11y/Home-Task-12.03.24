355e5dcea05de0ec8a033ce4dece45bc983a56ca
PainFu11y <162672019+PainFu11y@users.noreply.github.com>
2024-03-10 21:46:25 +0400

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-//        System.out.println("Nermuceq orva hamar@ :");
+//       Nermuceq orva hamar@ 
 //        int day = 1;
 //        switch (day){
 //            case 2:;
